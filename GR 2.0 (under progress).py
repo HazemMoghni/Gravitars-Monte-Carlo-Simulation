88a7563f15e5b50b6ab807e2d3f1a2c50b655b6c
HazemMoghni <80206463+HazemMoghni@users.noreply.github.com>
2023-08-20 19:48:23 +0300

    Update GR 2.0 (under progress).py

--- GR 2.0 (under progress).py
+++ GR 2.0 (under progress).py
@@ -19,17 +19,17 @@ Birthrate = 0.01  # 1 per 100 Yr
 Ti = 0  # Yr
 Tf = 100e6  # Yr
 N = 10  # Number of gravitars
-R_E = 8.2 * kpc  # m (Galactic-Earth distance)
-h_z = 0.075 * kpc  # m
-R_exp = 4.5 * kpc  # m
+R_E = 8.2 # kpc (Galactic-Earth distance)
+h_z = 0.075 # kpc
+R_exp = 4.5 # kpc
 a = 1.18285  # Dimensionless
 tau = -2.46493285798560e-28 # Dimensionless
 
 # Bounds
-ri_min = 0 * kpc  # m
-ri_max = 15 * kpc  # m
-zi_min = 0 * kpc  # m
-zi_max = float('inf')  # m
+ri_min = 0 # kpc
+ri_max = 15 # kpc
+zi_min = 0 # kpc
+zi_max = float('inf')  # kpc
 phi_min = 0  # rad
 phi_max = pi  # rad
 Pi_min = 0  # s
@@ -37,16 +37,21 @@ Pi_avg = 0.1  # s
 Pi_std = 0.1  # s
 Pi_max = float('inf')  # s
 age_min = 0  # s
-age_max = Tf * Yr  # s
+age_max = Tf # Yr
 ellipticity_min = 1e-9  # Dimensionless
 ellipticity_avg = 1e-8  # Dimensionless
 ellipticity_max = 1e-5  # Dimensionless
 
 
 class Gravitar:
-    def ri1(p):
-
-    def ri2(p):
+    
+    def find_ri(p, boolean):
+        def pdf_ri_p(ri, p):
+            return a * ri / math.pow(R_exp, 2) * math.exp(-ri / R_exp) - p
+        if boolean is True:
+            return root_scalar(pdf_ri_p, bracket=[ri_min, R_exp], args=(p,)).root
+        else:
+            return root_scalar(pdf_ri_p, bracket=[R_exp, ri_max], args=(p,)).root
 
     def zi(p):
         return h_z * math.log(1/(2 * h_z * p), e)
@@ -54,9 +59,14 @@ class Gravitar:
     def phi(p):
         return p * pi
 
-    def Pi(p):
-       # Did not solve yet
-
+    def find_Pi(p, boolean):
+        def pdf_Pi_p(Pi, p):
+            return a * ri / math.pow(R_exp, 2) * math.exp(-ri / R_exp) - p
+        if boolean is True:
+            return root_scalar(pdf_Pi_p, bracket=[ri_min, hmm], args=(p,)).root
+        else:
+            return root_scalar(pdf_ri_p, bracket=[hmm, ri_max], args=(p,)).root
+            
     def age(p):
         return p * Tf
 
